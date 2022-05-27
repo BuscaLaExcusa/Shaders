49e0b8861ba5bed87eb312fd3af17bc3133b2c21
BuscaLaExcusa <busca.la.excusa@gmail.com>
2022-05-27 09:08:55 +0200

    SimpleTerrainGenerator simplificado

--- Assets/Code/SimpleTerrainGenerator.cs
+++ Assets/Code/SimpleTerrainGenerator.cs
@@ -6,13 +6,10 @@ namespace BuscaLaExcusa
 {
     public class SimpleTerrainGenerator : MonoBehaviour
     {
-
         [SerializeField] protected MeshFilter _filter;
         [SerializeField] protected Vector2 _offset = Vector2.one;
-        [SerializeField] [Range(0f, 10f)] protected float _mountainHeight = 0.5f;
-        [SerializeField] [Range(0f, 10f)] protected float _mountainSpread = 0.2f;
-        [SerializeField] [Range(0f, 10f)] protected float _roughnessDepth = 0.5f;
-        [SerializeField] [Range(0f, 10f)] protected float _roughnessSpread = 0.2f;
+        [SerializeField] [Range(0f, 10f)] protected float _height = 0.5f;
+        [SerializeField] [Range(0f, 10f)] protected float _scale = 0.5f;
 
 
         private void Awake()
@@ -23,25 +20,22 @@ namespace BuscaLaExcusa
         private void GenerateTerrain()
         {
             Mesh mesh = _filter.mesh;
-
             Vector3[] vertices = mesh.vertices;
             for (int i = 0; i < vertices.Length; ++i)
             {
                 Vector3 vertex = vertices[i];
-
-                float x = _offset.x + vertex.x;
-                float z = _offset.y + vertex.z;
-
-                float mountainValue = _mountainHeight * Mathf.PerlinNoise(_mountainSpread * x, _mountainSpread * z);
-                float roughness = _roughnessDepth * Mathf.PerlinNoise(_roughnessSpread * x, _roughnessSpread * z);
-
-                vertex.y = mountainValue;
-                vertex.y += Mathf.Abs(roughness);
-
+                vertex.y = GetHeight(vertex.x, vertex.z);
                 vertices[i] = vertex;
             }
             mesh.vertices = vertices;
             mesh.RecalculateNormals();
+        }
+
+        private float GetHeight(float x, float z)
+        {
+            x += _offset.x;
+            z += _offset.y;
+            return Mathf.PerlinNoise(x / _filter.mesh.bounds.size.x * _scale, z / _filter.mesh.bounds.size.z * _scale) * _height;
         }
 
         private void OnDrawGizmos()
